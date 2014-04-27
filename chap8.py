882998cc8605dad029ea9cc95ed6293cf0f2dada
tywallace <tylerwallace@gmail.com>
2014-04-27 13:11:56 -0400

    rot13 single word only

--- chap8.py
+++ chap8.py
@@ -1,2 +1,14 @@
 # This is where the answers to Chapter 8 questions for the BSS Dev RampUp go
-# Name:
+# Name: Tyler
+
+def rot13(string):
+	new_string = ""
+	for x in string:
+		if ord(x) < 97:
+			y = chr(((ord(x)-65 + 13) % 26) + 65)
+		else:
+			y = chr(((ord(x)-97 + 13) % 26) + 97)
+		new_string = new_string + y
+	print new_string
+
+rot13("HeLlo")
