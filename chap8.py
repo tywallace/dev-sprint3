7d7d0a3fad820cb9855097c2847e96a1276b2363
tywallace <tylerwallace@gmail.com>
2014-04-30 16:44:19 -0400

    Adapted rot13 to handle files
    It doesn't exactly handle spaces but it seems to work.

--- chap8.py
+++ chap8.py
@@ -4,11 +4,28 @@
 def rot13(string):
 	new_string = ""
 	for x in string:
-		if ord(x) < 97:
+		if ord(x) < 91 and ord(x) > 64:
 			y = chr(((ord(x)-65 + 13) % 26) + 65)
-		else:
+		elif ord(x) < 123 and ord(x) > 96:
 			y = chr(((ord(x)-97 + 13) % 26) + 97)
+		else:
+			y = x
 		new_string = new_string + y
-	print new_string
+	return new_string
 
 rot13("HeLlo")
+
+f = open('Rot13Test.txt', 'r')
+d = open('Decoded.txt', 'w')
+
+
+for line in f:
+ 	l = line
+ 	new_line = ""
+ 	for word in l.split():
+ 		new_line = new_line + rot13(word) + " "
+ 	d.write(new_line + "\n")
+
+
+
+
